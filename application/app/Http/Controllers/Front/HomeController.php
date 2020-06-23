4e5022ffa316de99743b6d253af0d9813de457ff
nakama-t <nakama-t@illmatics.co.jp>
2020-06-23 13:29:39 +0900

    first commit

---
+++ application/app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('front.home');
+    }
+}
---
+++ application/routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/**
+ * フロントサイド
+ */
+Route::namespace('Front')->name('front.')->group(function () {
+    Auth::routes([
+        'register' => true,
+        'reset' => false,
+        'verify' => false,
+    ]);
+
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+/**
+ * 管理サイド
+ */
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Auth::routes([
+        'register' => false,
+        'reset' => false,
+        'verify' => false,
+    ]);
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('home', 'HomeController@index')->name('home');
+    });
+});
+
+/**
+ * リダイレクト
+ */
+Route::redirect('/', '/home');
+Route::redirect('/admin', '/admin/home');
